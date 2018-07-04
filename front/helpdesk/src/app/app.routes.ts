36985a6a30e9852787ccc90ef322e4aae5460ccf
cspassos <cspassos@latam.stefanini.com>
2018-07-04 11:32:19 -0300

    Criando Componente UserNew parte 02 - tela para cadastro de usuario

--- front/helpdesk/src/app/app.routes.ts
+++ front/helpdesk/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { ModuleWithProviders } from '@angular/core';
 import { LoginComponent } from './components/security/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { AuthGuard } from './components/security/auth.guard';
+import { UserNewComponent } from './components/user-new/user-new.component';
 
 export const ROUTES: Routes = [
     { path : '', component: HomeComponent, canActivate: [AuthGuard]}, //rota inicial
-    { path : 'login', component: LoginComponent }
+    { path : 'login', component: LoginComponent },
+    { path : 'user-new', component: UserNewComponent, canActivate: [AuthGuard] }
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(ROUTES);
